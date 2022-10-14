cd56aa6ba0f8b494170b092ad15cac99f19fbd1b
Artur Santos <arturfrancisco86@gmail.com>
2022-10-14 09:00:35 -0300

    Organizando repo

--- teste1/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ teste1/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -14,7 +14,7 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10001")));
 
 		assertEquals(new BigDecimal("0.00"), bonus);
 	}
